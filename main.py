70f6b029819dd51ca1965e8366b17f51170af6cc
Henryle-hd <henrydionizi@gmail.com>
2024-10-31 12:21:35 +0300

    v1.1

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from dM import llm_response
 st.write("# Doctor Mtandao!")
+# st.markdown("###### What to test?")
+st.sidebar.markdown("What to test?")
+st.sidebar.markdown("Upload test results")
+
 symptoms=st.text_input(label="Enter symptom...")
 
 if st.button("Submit"):
@@ -8,5 +12,7 @@ if st.button("Submit"):
         result=llm_response(symptoms)
         st.write("# Doctor")
         st.write(result)
+    else:
+        st.write("Please enter your symptom...")
 
 
